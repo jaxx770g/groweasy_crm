cb54fec8ea29901f9fa4b1239b02104b98239ac0
jaxx770g <jays86922@gmail.com>
2026-07-11 23:09:19 +0530

    Fix wildcard route syntax for Express 5

--- server/app.js
+++ server/app.js
@@ -14,7 +14,7 @@ app.use(cors({
   credentials: true,
 }));
 
-app.options('*', cors());
+app.options('/*splat', cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
